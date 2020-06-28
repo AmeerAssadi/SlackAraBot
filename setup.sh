e32de349994e4a3fcf400b5bb9ed72baf9571751
Ameer Assadi <ameerassadime@gmail.com>
2020-06-28 18:22:54 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-git clone https://github.com/AmeerAssadi/SlackAraBot.git
-chmod -R 777 SlackAraBot
-echo "* * * * * python $PWD/AraBot/bot.py" >> /etc/crontab
+echo "* * * * * python $PWD/SlackAraBot/bot.py" >> /etc/crontab
 
 echo -e "${GREEN}Everything is ready, mate!${NC}"
